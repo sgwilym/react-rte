ca1ab1a60952eaa43bdb6bd15f56868b5a8884e2
Samuel Gwilym <sam@comique.co>
2017-05-25 13:48:52 +0200

    🤷‍♂️

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,18 @@ var loaders = [
 ];
 
 module.exports = [{
+  entry: './src/RichTextEditor.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'react-rte.js',
+    libraryTarget: 'commonjs2',
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+  },
+  module: {loaders: loaders},
+}, {
   entry: {
     demo: './src/demo.js',
   },
